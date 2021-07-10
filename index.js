f88b8a5660cd7df7e045dffd1b7307c1b996291c
3STEB4N28 <71854065+3STEB4N28@users.noreply.github.com>
2021-07-10 12:12:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const { checkUpdates, apiOnline } = require('./util.js');
 const { get } = require("axios");
 const colors = require("colors");
 const url = "https://api.berk404.ga/check?link=";
+
+apiOnline().then(b => {
+    if(b === true) {
+        console.log(`${colors.red('[Berk-api.js]')} Successfully connected to the api.\n${colors.red('[Berk-api.js]')} For more support join our Discord server: ${colors.blue('https://discord.gg/uGUZshHcgV')}!`);
+    } else if(b === false) {
+        console.log(`${colors.red('[Berk-api.js]')} API is currently offline!\n${colors.red('Berk-api.js]')} Please check the Discord server for more information! ${colors.blue('(https://discord.gg/uGUZshHcgV)')}`);
+    }
+});
 
 async function request(endpoint, params) {
     let d;
@@ -14,7 +23,7 @@ async function request(endpoint, params) {
 }
 
 function encodeText(text) {
-    let r = text.split(/ +/g).join('+').replace(/#/g, '%23').replace(/=/g, '%3D').replace(/!/g, '%21')
+    let r = text.split(/ +/g).join('+').replace(/#/g, '%23').replace(/=/g, '%3D').replace(/!/g, '%21');
 
     return r;
 }
@@ -30,12 +39,12 @@ module.exports = {
     version: require('./package.json').version,
     get info() {
         return({
-            "version": require("./package.json").version,
+            "version": require('./package.json').version,
             "name": "berk-api.js",
             "source": "https://api.berk404.ga/",
             "language": "javascript"
         })
-    }, //Falsis :)))
+    },
     async overlay(type, image) {
         if(!type) {
             return console.log(`${colors.red('[Berk-api.js]')} Function ${colors.blue('overlay')} is missing a ${colors.yellow('type')}!`);
@@ -51,9 +60,9 @@ module.exports = {
 
         const a = await request(type, image);
 
-        return encodeAnswer(a.data.image);
+        return await encodeAnswer(a.data.image);
     },
-    async anycard(image, {text1 = undefined, text2 = undefined, text3 = undefined}, {color1, color2, color3}, background) {
+    async anycard({image, circle = false}, {text1 = undefined, text2 = undefined, text3 = undefined}, {color1, color2, color3}, background) {
         if(!image) {
             console.log(`${colors.red('[Berk-api.js]')} Function ${colors.blue('anycard')} is missing an ${colors.yellow('image')}!`);
             
@@ -76,20 +85,23 @@ module.exports = {
         }
 
         let a;
-
-        let url = `avatar=${image}%26text1=` + await encodeText(text1) + `%26text2=` + await encodeText(text2) +`%26text3=` + await encodeText(text3);
+        
+        let url;
+        
+        if(circle === true) url = `avatar=https://api.berk404.ga/circle%3Fimage=${image}%26text1=` + await encodeText(text1) + `%26text2=` + await encodeText(text2) +`%26text3=` + await encodeText(text3);
+        else url = `avatar=${image}%26text1=` + await encodeText(text1) + `%26text2=` + await encodeText(text2) +`%26text3=` + await encodeText(text3);
 
         if(background) {
             if(color1) {
-                a = await request('anycard', `${url}&background=${background}&color1=${color1}`);
+                a = await request('anycard', `${url}%26background=${background}%26color1=${color1}`);
             }
 
             if(color2) {
-                a = await request('anycard', `${url}&background=${background}&color1=${color1}&color2=${color2}`);
+                a = await request('anycard', `${url}%26background=${background}%26color1=${color1}%26color2=${color2}`);
             }
 
             if(color3) {
-                a = await request('anycard', `${url}&background=${background}&color1=${color1}&coolor2=${color2}&color3=${color3}`);
+                a = await request('anycard', `${url}%26background=${background}%26color1=${color1}%26coolor2=${color2}%26color3=${color3}`);
             }
         } else {
             if(color1) {
@@ -105,7 +117,7 @@ module.exports = {
             }
         }
 
-        return encodeAnswer(a.data.image);
+        return await encodeAnswer(a.data.image);
     },
     async pablo(text, everyone = false) {
         if(!text) {
@@ -122,18 +134,21 @@ module.exports = {
             a = await request('pablo', `text=${await encodeText(text)}`);
         }
 
-        return encodeAnswer(a.data.image);
+        return await encodeAnswer(a.data.image);
     },
-    async whoreallyis(image) {
+    async whoreallyis(image, circle = false) {
         if(!image) {
             console.log(`${colors.red('[Berk-api.js]')} Function ${colors.blue('whoreallyis')} is missing an ${colors.yellow('image')}!`);
     
             return;
         }
 
-        let a = await request('whoreallyis', `image=${image}`);
+        let a;
+        
+        if(false === true) a = await request('whoreallyis', `image=https://api.berk404.ga/circle%3Fimage=${image}`);
+        else a = await request('whoreallyis', 'image=${image}');
 
-        return encodeAnswer(a.data.image);
+        return await encodeAnswer(a.data.image);
     },
     async poohsay(text1, text2) {
         if(!text1) {
@@ -149,7 +164,7 @@ module.exports = {
 
         let a = await request('poohsay', `text1=${await encodeText(text1)}&text2=${await encodeText(text2)}`);
 
-        return encodeAnswer(a.data.image);
+        return await encodeAnswer(a.data.image);
     },
     async angrymob(text1, text2) {
         if(!text1) {
@@ -165,17 +180,36 @@ module.exports = {
 
         let a = await request('angrymob', `text1=${await encodeText(text1)}&text2=${await encodeText(text2)}`);
 
-        return encodeAnswer(a.data.image);
+        return await encodeAnswer(a.data.image);
     },
-    async uglier(image) {
+    async uglier(image, circle = false) {
         if(!image) {
             console.log(`${colors.red('[Berk-api.js]')} Function ${colors.blue('uglier')} is missing an ${colors.yellow('image')}!`);
     
             return;
         }
 
-        let a = await request('uglier', `image=${image}`);
+        let a;
+        if(circle === true) a = await request('uglier', `image=https://api.berk404.ga/circle%3Fimage=${image}`);
+        else a = await request('uglier', `image=${image}`);
 
-        return encodeAnswer(a.data.image);
+        return await encodeAnswer(a.data.image);
+    },
+    async sus(impostor, crewmate) {
+        if(!impostor) {
+            console.log(`${colors.red('[Berk-api.js]')} Function ${colors.blue('sus')} is missing an ${colors.yellow('impostor')}!`);
+    
+            return;
+        }
+
+        if(!crewmate) {
+            console.log(`${colors.red('[Berk-api.js]')} Function ${colors.blue('sus')} is missing an ${colors.yellow('crewmate')}!`);
+    
+            return;
+        }
+
+        let a = await request('sus', `impostor=${impostor}%26crewmate=${crewmate}`);
+
+        return await encodeAnswer(a.data.image)
     }
 }
