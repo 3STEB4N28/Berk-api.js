3a65dc77ec4965f4891a82e40db571f295593b49
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-10 17:24:55 +0300

    add: added test file

---
+++ test.js
@@ -0,0 +1,6 @@
+const berk = require("./index.js");
+console.log(berk.info)
+console.log(berk.info.version)
+console.log(berk.info.name)
+console.log(berk.info.source)
+console.log(berk.info.language)
